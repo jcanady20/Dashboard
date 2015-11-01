5d173ce5e56365e02a0edd2254902ffb69fa17b1
James Canady <jcanady20@gmail.com>
2015-11-01 08:27:51 -0600

    UI Work

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,22 @@
 $(function () {
-    Morris.Area({
+    Morris.Line({
         element: 'm-chart-line-1',
         data: [
-            { y: '2006', a: 100, b: 90 },
-            { y: '2007', a: 75, b: 65 },
-            { y: '2008', a: 50, b: 40 },
-            { y: '2009', a: 75, b: 65 },
-            { y: '2010', a: 50, b: 40 },
-            { y: '2011', a: 75, b: 65 },
-            { y: '2012', a: 100, b: 90 }
+            { y: '2006', a: 20 },
+            { y: '2007', a: 25 },
+            { y: '2008', a: 30 },
+            { y: '2009', a: 45 },
+            { y: '2010', a: 50 },
+            { y: '2011', a: 65 },
+            { y: '2012', a: 75 },
+            { y: '2013', a: 45 },
+            { y: '2014', a: 50 }
         ],
         xkey: 'y',
-        ykeys: ['a', 'b'],
-        labels: ['Series A', 'Series B'],
+        ykeys: ['a'],
+        labels: ['Series A'],
         resize: true
     });
-
     Morris.Donut({
         element: 'm-chart-circle-1',
         labelColor: "#fff",
